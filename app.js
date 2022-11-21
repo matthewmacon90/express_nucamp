26fa8bf797e1f492f174b89c5ce09291a3eef428
Matthew Macon <matthewmacon90@gmail.com>
2022-11-21 14:26:37 -0500

    HTTPS and Secure Communication

--- app.js
+++ app.js
@@ -25,8 +25,16 @@ connect.then(() => console.log('Conected correctly to server'),
     err => console.log(err)
 );
 
-
 var app = express();
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
