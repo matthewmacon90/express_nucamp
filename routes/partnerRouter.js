30030886b39b69baf80ebc82897e522d95985b5f
Matthew Macon <matthewmacon90@gmail.com>
2022-11-22 14:34:33 -0500

    Cross-Origin Resource Sharing

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const partnerRouter = express.Router();
 const authenticate = require('../authenticate');
+const cors = require('./cors');
 
 const Partner = require('../models/partner');
 
 partnerRouter.route('/')
-.get((req, res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, (req, res, next) => {
     Partner.find()
     .then(partners => res.status(200).json(partners)) //Another possible way
     .catch(err => next(err))
 })
-.post(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
     Partner.create(req.body)
     .then(partner => res.status(200).json(partner))
     .catch(err => next(err))
     // console.log('Partner Created ', partner);
 })
-.put(authenticate.verifyUser,(req, res) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /partners');
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
     Partner.deleteMany()
     .then(partners => res.status(200).json(partners))
     .catch(err => next(err))
@@ -28,17 +30,18 @@ partnerRouter.route('/')
 });
 
 partnerRouter.route('/:partnerId')
-.get((req,res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, (req, res, next) => {
     Partner.findById(req.params.partnerId)
     .then(partner => res.status(200).json(partner))
     .catch(err => next(err))
     // console.log('Partners Deleted ', partner);
 })
-.post(authenticate.verifyUser,(req, res) => { //Can delete...you will never support a post here and waste of resouce...notes for future.
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => { //Can delete...you will never support a post here and waste of resouce...notes for future.
     res.statusCode = 403;
     res.end(`POST operation not supported on /partners/:partnerId`);
 })
-.put(authenticate.verifyUser, authenticate.verifyAdmin,(req,res, next) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req,res, next) => {
     Partner.findByIdAndUpdate(req.params.partnerId, {
         $set: req.body //replaces entire body or certain fields{description: req.body.description,  name: req.body.name}
     }, {new: true}) //Returning payload..False stops the .then
@@ -46,7 +49,7 @@ partnerRouter.route('/:partnerId')
     .catch(err => next(err))
     // console.log('Partners Deleted ', partner);
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin,(req,res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req,res, next) => {
     Partner.findByIdAndDelete(req.params.partnerId)
     .then(partner => res.status(200).json(partner))
     .catch(err => next(err))
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const promotionRouter = express.Router();
 const authenticate = require('../authenticate');
+const cors = require('./cors');
 
 const Promotion = require('../models/promotion');
 
 promotionRouter.route('/')
-.get((req, res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, (req, res, next) => {
     Promotion.find()
     .then(promotions => res.status(200).json(promotions)) //Another possible way
     .catch(err => next(err))
 })
-.post(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
     Promotion.create(req.body)
     .then(promotion => res.status(200).json(promotion))
     .catch(err => next(err))
     // console.log('Promotion Created ', promotion);
 })
-.put(authenticate.verifyUser,(req, res) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
     Promotion.deleteMany()
     .then(promotions => res.status(200).json(promotions))
     .catch(err => next(err))
@@ -28,17 +30,18 @@ promotionRouter.route('/')
 });
 
 promotionRouter.route('/:promotionId')
-.get((req,res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, (req, res, next) => {
     Promotion.findById(req.params.promotionId)
     .then(promotion => res.status(200).json(promotion))
     .catch(err => next(err))
     // console.log('Promotions Deleted ', promotion);
 })
-.post(authenticate.verifyUser,(req, res) => { //Can delete...you will never support a post here and waste of resouce...notes for future.
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => { //Can delete...you will never support a post here and waste of resouce...notes for future.
     res.statusCode = 403;
     res.end(`POST operation not supported on /promotions/:promotionId`);
 })
-.put(authenticate.verifyUser, authenticate.verifyAdmin,(req,res, next) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req,res, next) => {
     Promotion.findByIdAndUpdate(req.params.promotionId, {
         $set: req.body //replaces entire body or certain fields{description: req.body.description,  name: req.body.name}
     }, {new: true}) //Returning payload..False stops the .then
@@ -46,7 +49,7 @@ promotionRouter.route('/:promotionId')
     .catch(err => next(err))
     // console.log('Promotions Deleted ', promotion);
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin,(req,res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req,res, next) => {
     Promotion.findByIdAndDelete(req.params.promotionId)
     .then(promotion => res.status(200).json(promotion))
     .catch(err => next(err))
